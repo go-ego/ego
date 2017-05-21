5264c19efa5ae71f3dd96b9c1b37ed86c445dc7e
vCaesar <vzvway@gmail.com>
2017-05-21 16:47:53 +0800

    Try move logger

---
+++ mid/recovery_test.go
@@ -0,0 +1,44 @@
+// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package mid
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/go-ego/ego"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestPanicInHandler assert that panic has been recovered.
+func TestPanicInHandler(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	router := ego.New()
+	router.Use(RecoveryWithWriter(buffer))
+	router.GET("/recovery", func(_ *ego.Context) {
+		panic("Oupps, Houston, we have a problem")
+	})
+	// RUN
+	w := performRequest(router, "GET", "/recovery")
+	// TEST
+	assert.Equal(t, w.Code, 500)
+	assert.Contains(t, buffer.String(), "GET /recovery")
+	assert.Contains(t, buffer.String(), "Oupps, Houston, we have a problem")
+	assert.Contains(t, buffer.String(), "TestPanicInHandler")
+}
+
+// TestPanicWithAbort assert that panic has been recovered even if context.Abort was used.
+func TestPanicWithAbort(t *testing.T) {
+	router := ego.New()
+	router.Use(RecoveryWithWriter(nil))
+	router.GET("/recovery", func(c *ego.Context) {
+		c.AbortWithStatus(400)
+		panic("Oupps, Houston, we have a problem")
+	})
+	// RUN
+	w := performRequest(router, "GET", "/recovery")
+	// TEST
+	assert.Equal(t, w.Code, 400)
+}

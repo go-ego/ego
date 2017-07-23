dc582c22eb94d397f6dd0acd27916f4700f5174f
vCaesar <vzvway@gmail.com>
2017-07-23 15:16:33 +0800

    Add template example

---
+++ examples/template/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/go-ego/ego"
+)
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d%02d/%02d", year, month, day)
+}
+
+func main() {
+	router := ego.Default()
+	router.Delims("{[{", "}]}")
+	router.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+	router.LoadHTMLFiles("../../test/basic/raw.tmpl")
+
+	router.GET("/raw", func(c *ego.Context) {
+		c.HTML(http.StatusOK, "raw.tmpl", map[string]interface{}{
+			"now": time.Date(2017, 07, 01, 0, 0, 0, 0, time.UTC),
+		})
+	})
+
+	router.Run(":8080")
+}

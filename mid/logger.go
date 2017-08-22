0b0f35c2facfe71737897cb7103fc56ab70160c2
vcaesar <vzvway@gmail.com>
2017-08-22 20:24:24 +0800

    not display color

--- mid/logger.go
+++ mid/logger.go
@@ -96,10 +96,11 @@ func LoggerWithWriter(out io.Writer, notlogged ...string) ego.HandlerFunc {
 			// method := Colorize(c.Request.Method, "note")
 			method := c.Request.Method
 			statusCode := c.Writer.Status()
-			var statusColor, methodColor string
+			var statusColor, methodColor, resetColor string
 			if isTerm {
 				statusColor = colorForStatus(statusCode)
 				methodColor = colorForMethod(method)
+				resetColor = reset
 			}
 			comment := c.Errors.ByType(util.ErrorTypePrivate).String()
 
@@ -109,10 +110,10 @@ func LoggerWithWriter(out io.Writer, notlogged ...string) ego.HandlerFunc {
 			// fmt.Fprintf(out, "[EGO] %v |%s %3d %s| %13v | %15s |%s  %s %-7s %s\n%s",
 			fmt.Fprintf(out, "[EGO] %v |%s %3d %s| %13v | %15s |%s %-7s %s %s\n%s",
 				end.Format("2006/01/02 - 15:04:05"),
-				statusColor, statusCode, reset,
+				statusColor, statusCode, resetColor,
 				latency,
 				clientIP,
-				methodColor, method, reset,
+				methodColor, method, resetColor,
 				path,
 				comment,
 			)
